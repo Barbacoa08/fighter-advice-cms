ad92c0217d1fe5ec0ed92b6b4cf0ceab1e2d80dc
barbajoe <sabe12070@gmail.com>
2023-01-16 16:46:22 -0600

    add "roles" to `Users`

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -22,6 +22,24 @@ const Users: CollectionConfig = {
       name: "name",
       type: "text",
     },
+    {
+      name: "roles",
+      // Save this field to JWT so we can use from `req.user`
+      saveToJWT: true,
+      type: "select",
+      hasMany: true,
+      defaultValue: ["editor"],
+      options: [
+        {
+          label: "Admin",
+          value: "admin",
+        },
+        {
+          label: "Editor",
+          value: "editor",
+        },
+      ],
+    },
   ],
 };
 
